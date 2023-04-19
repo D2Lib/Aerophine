2ee9a155ee412c0ba324cfdba2728ad4dc383ec6
ArthurZhou <yitian.zhou@icloud.com>
2023-04-19 19:32:49 +0800

    20230419

--- js/message.js
+++ js/message.js
@@ -1,19 +1,19 @@
-function showMsg(msg, color="#e6e6e6", bgcolor="#e34a4a") {
+function showMsg(msg, color="#e6e6e6", bgColor="#e34a4a") {
     globColor = color
-    globBgcolor = bgcolor
+    globBgColor = bgColor
 
-    if (document.getElementById("message").style.top === '-20.5px') {
-        display(msg, color, bgcolor)
+    if (document.getElementById("message").style.top === '-20px') {
+        display(msg, color, bgColor)
     } else {
         hideMsg()
         setTimeout(function() {
-            display(msg, color, bgcolor)
+            display(msg, color, bgColor)
         }, 200);
     }
 }
 
-function display(msg, color, bgcolor) {
-    setColor(color, bgcolor)
+function display(msg, color, bgColor) {
+    setColor(color, bgColor)
     document.getElementById("message").innerHTML = '<div class="inner">'
         + msg + '<a onclick="hideMsg()" class="close-btn">' +
         '<svg id="close-svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke="' + color + '" fill="none" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>' +
@@ -24,21 +24,19 @@ function display(msg, color, bgcolor) {
     }, 40);
 }
 
-function setColor(color, bgcolor) {
+function setColor(color, bgColor) {
     document.getElementById("message").style.transition = "none 0s ease 0s"
-    document.getElementById("message").style.backgroundColor = bgcolor
+    document.getElementById("message").style.backgroundColor = bgColor
     document.getElementById("message").style.color = color
     setTimeout(function() {document.getElementById("message").style.transition = 'all 0.2s ease 0s'}, 30);
 }
 
 function hideMsg() {
-    document.getElementById("message").style.top = "-20.5px"
+    document.getElementById("message").style.top = "-20px"
     document.getElementById("nav").style.top = "0"
 }
 
-let globColor = "#e6e6e6";
-let globBgcolor = "#e34a4a";
-
+let globColor = "#e6e6e6", globBgColor = "#e34a4a";
 window.addEventListener("DOMContentLoaded", () => {
     const isHover = e => e.parentElement.querySelector(':hover') === e;
 
@@ -47,9 +45,9 @@ window.addEventListener("DOMContentLoaded", () => {
         if (hovered !== checkHover.hovered) {
             if (hovered) {
                 document.getElementById("close-svg").style.backgroundColor = globColor
-                document.getElementById("close-svg").style.stroke = globBgcolor
+                document.getElementById("close-svg").style.stroke = globBgColor
             } else {
-                document.getElementById("close-svg").style.backgroundColor = globBgcolor
+                document.getElementById("close-svg").style.backgroundColor = globBgColor
                 document.getElementById("close-svg").style.stroke = globColor
             }
             checkHover.hovered = hovered;
