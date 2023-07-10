b1cba6ad2a059657a6ab4602af364bdb0cb20888
ArthurZhou <yitian.zhou@icloud.com>
2023-07-10 20:16:56 +0800

    20230710

---
+++ js/categories.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("toc").style.left = "-400px"
+    let toc = "";
+    let level = 0;
+
+    document.getElementById("contents").innerHTML =
+        document.getElementById("contents").innerHTML.replace(
+            /<h([\d])>([^<]+)<\/h([\d])>/gi,
+            function (str, openLevel, titleText, closeLevel) {
+                if (openLevel !== closeLevel) {
+                    return str;
+                }
+
+                if (openLevel > level) {
+                    toc += (new Array(openLevel - level + 1)).join("<ul>");
+                } else if (openLevel < level) {
+                    toc += (new Array(level - openLevel + 1)).join("</ul>");
+                }
+
+                level = parseInt(openLevel);
+
+                const anchorTag = titleText.replace(/ /g, "_");
+                toc += "<li><a href=\"#" + anchorTag + "\">" + titleText
+                    + "</a></li>";
+
+                return `<h${openLevel}><a class="toc-title" id="${anchorTag}">${titleText}</a></h${closeLevel}>`;
+            }
+        );
+
+    if (level) {
+        toc += (new Array(level + 1)).join("</ul>");
+    }
+
+    document.getElementById("toc").innerHTML += toc;
+});
+
+function toc() {
+    console.log(document.getElementById("toc").style.left)
+    if (document.getElementById("toc").style.left === "0" || document.getElementById("toc").style.left === "0px") {
+        document.getElementById("toc").style.left = "-400px"
+        document.getElementById("toc-btn").innerHTML = " &#9654; "
+    } else {
+        document.getElementById("toc").style.left = "0"
+        document.getElementById("toc-btn").innerHTML = " &#9664; "
+    }
+}
